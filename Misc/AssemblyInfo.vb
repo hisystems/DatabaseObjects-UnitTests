da14831f0936f4c53cdaa7ca878eb29402a3a7f2
Toby Wicks <toby@hisystems.com.au>
2012-03-04 10:18:12 +1030

    Initial Commit - DatabaseObjects.UnitTests blank testing project for DatabaseObjects library.

---
+++ Misc/AssemblyInfo.vb
@@ -0,0 +1,15 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("DatabaseObjects.UnitTests")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("DatabaseObjects.UnitTests")> 
+<Assembly: AssemblyCopyright("Copyright ©  2012")> 
+<Assembly: AssemblyTrademark("")> 
+<Assembly: CLSCompliant(True)> 
+<Assembly: ComVisible(False)> 
+<Assembly: Guid("e8f4c302-5fd9-4e53-a3cc-433c658d73ec")> 
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
