3742451f0541e01fdf17f75f695897a2ccbd1d04
Toby Wicks <toby@hisystems.com.au>
2012-03-04 12:38:06 +1030

    Added tests for SQLBitwiseExpressions
    Associated commit DatabaseObjects:c9cf306babed5a67535ed712830fd1a3f981a331

---
+++ SQL/SQLBitwiseExpressionTest.vb
@@ -0,0 +1,57 @@
+﻿Imports System.Text
+Imports DatabaseObjects.SQL
+
+<TestClass()>
+Public Class SQLBitwiseExpressionTest
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("SQLExpression")>
+    Public Sub BitwiseAndOperator()
+
+        Dim objBitwiseOperator As New SQLBitwiseExpression()
+        objBitwiseOperator.LeftExpression = New SQLValueExpression(1)
+        objBitwiseOperator.Operator = BitwiseOperator.And
+        objBitwiseOperator.RightExpression = New SQLValueExpression(2)
+
+        Assert.AreEqual(Of String)(objBitwiseOperator.SQL(Database.ConnectionType.SQLServer), "(1 & 2)")
+
+    End Sub
+
+    <TestMethod()> _
+    <TestCategory("SQL"), TestCategory("SQLExpression")>
+    Public Sub BitwiseOrOperator()
+
+        Dim objBitwiseOperator As New SQLBitwiseExpression()
+        objBitwiseOperator.LeftExpression = New SQLValueExpression(1)
+        objBitwiseOperator.Operator = BitwiseOperator.Or
+        objBitwiseOperator.RightExpression = New SQLValueExpression(2)
+
+        Assert.AreEqual(Of String)(objBitwiseOperator.SQL(Database.ConnectionType.SQLServer), "(1 | 2)")
+
+    End Sub
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("SQLExpression")>
+    <ExpectedException(GetType(ArgumentNullException))>
+    Public Sub BitwiseOperatorRightExpressionNotSet()
+
+        Dim objBitwiseOperator As New SQLBitwiseExpression()
+        objBitwiseOperator.LeftExpression = New SQLValueExpression(1)
+
+        objBitwiseOperator.SQL(Database.ConnectionType.SQLServer)
+
+    End Sub
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("SQLExpression")>
+    <ExpectedException(GetType(ArgumentNullException))>
+    Public Sub BitwiseOperatorLeftExpressionNotSet()
+
+        Dim objBitwiseOperator As New SQLBitwiseExpression()
+        objBitwiseOperator.RightExpression = New SQLValueExpression(1)
+
+        objBitwiseOperator.SQL(Database.ConnectionType.SQLServer)
+
+    End Sub
+
+End Class
