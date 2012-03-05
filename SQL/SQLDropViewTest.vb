09c9df5e8bef21f81b036078575d7aa2f7d68aad
Toby Wicks <toby@hisystems.com.au>
2012-03-05 20:46:55 +1030

    Added tests for SQLDropView
    Associated commit DatabaseObjects:29c2f767772838ab5ce48e661c27296fd7d32d54

---
+++ SQL/SQLDropViewTest.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Text
+Imports DatabaseObjects.SQL
+
+<TestClass()>
+Public Class SQLDropViewTest
+
+    Public TestContext As TestContext
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("Views")>
+    Public Sub DropView()
+
+        SQLStatement.DefaultConnectionType = Database.ConnectionType.SQLServer
+
+        Dim dropView As New SQLDropView("ViewName")
+
+        Assert.AreEqual(Of String)("DROP VIEW [ViewName]", dropView.SQL)
+
+    End Sub
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("Views")>
+    <ExpectedException(GetType(ArgumentNullException))>
+    Public Sub DropViewWithInvalidName()
+
+        Dim view As New SQLDropView("")
+
+    End Sub
+
+End Class
