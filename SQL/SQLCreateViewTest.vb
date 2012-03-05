fc62f9b448cf84ffa78281d5dbe455fd3c089ef7
Toby Wicks <toby@hisystems.com.au>
2012-03-05 20:38:45 +1030

    Added tests for SQLCreateView
    Associated commit DatabaseObjects:df35b3e6771835bbd597016fc832c39d66135484

---
+++ SQL/SQLCreateViewTest.vb
@@ -0,0 +1,57 @@
+﻿Imports System.Text
+Imports DatabaseObjects.SQL
+
+<TestClass()>
+Public Class SQLCreateViewTest
+
+    Public TestContext As TestContext
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("Views")>
+    Public Sub CreateView()
+
+        SQLStatement.DefaultConnectionType = Database.ConnectionType.SQLServer
+
+        Dim view As New SQLCreateView
+        view.Name = "ViewName"
+        view.Select = New SQLSelect("Table1")
+
+        Assert.AreEqual(Of String)("CREATE VIEW [ViewName] AS SELECT * FROM [Table1]", view.SQL)
+
+    End Sub
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("Views")>
+    Public Sub CreateViewUsingConstructor()
+
+        SQLStatement.DefaultConnectionType = Database.ConnectionType.SQLServer
+
+        Dim view As New SQLCreateView("ViewName", New SQLSelect("Table1"))
+
+        Assert.AreEqual(Of String)("CREATE VIEW [ViewName] AS SELECT * FROM [Table1]", view.SQL)
+
+    End Sub
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("Views")>
+    <ExpectedException(GetType(Exceptions.DatabaseObjectsException))>
+    Public Sub CreateViewWithInvalidName()
+
+        Dim view As New SQLCreateView
+        Dim sql As String = view.SQL()
+
+    End Sub
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("Views")>
+    <ExpectedException(GetType(Exceptions.DatabaseObjectsException))>
+    Public Sub CreateViewWithInvalidSelectStatement()
+
+        Dim view As New SQLCreateView
+        view.Name = "ViewName"
+
+        Dim sql As String = view.SQL()
+
+    End Sub
+
+End Class
