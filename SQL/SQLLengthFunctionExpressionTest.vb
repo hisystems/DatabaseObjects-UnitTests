b42de00c6f1f423a63e58e29e981e4415ab5f26a
Toby Wicks <toby@hisystems.com.au>
2012-03-05 21:59:24 +1030

    Added tests for SQLLengthFunctionExpression.
    Associated commit DatabaseObjects:d5dd0fc31af5583b28c173f080c74851aea1e4a0

---
+++ SQL/SQLLengthFunctionExpressionTest.vb
@@ -0,0 +1,36 @@
+﻿Imports System.Text
+Imports DatabaseObjects.SQL
+
+<TestClass()>
+Public Class SQLLengthFunctionExpressionTest
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("SQLExpression")>
+    Public Sub SQLLengthFunctionExpressionTSQL()
+
+        Dim lengthFunction As New SQLLengthFunctionExpression(New SQLValueExpression("123"))
+
+        Assert.AreEqual(Of String)(lengthFunction.SQL(Database.ConnectionType.SQLServer), "LEN('123')")
+
+    End Sub
+
+    <TestMethod()> _
+    <TestCategory("SQL"), TestCategory("SQLExpression")>
+    Public Sub SQLLengthFunctionExpressionMySQL()
+
+        Dim lengthFunction As New SQLLengthFunctionExpression(New SQLValueExpression("123"))
+
+        Assert.AreEqual(Of String)(lengthFunction.SQL(Database.ConnectionType.MySQL), "LENGTH('123')")
+
+    End Sub
+
+    <TestMethod()> _
+    <TestCategory("SQL"), TestCategory("SQLExpression")>
+    <ExpectedException(GetType(ArgumentNullException))>
+    Public Sub SQLLengthFunctionExpressionInvalidArgument()
+
+        Dim lengthFunction As New SQLLengthFunctionExpression(Nothing)
+
+    End Sub
+
+End Class
