a96906c69a5732e45eb5986fd626d0f05a205c94
Toby Wicks <toby@hisystems.com.au>
2012-03-05 21:13:41 +1030

    Added tests for SQLViewExists
    Associated commit DatabaseObjects:de3d660e598b791ba810834e296f8fb14f5abd11

---
+++ SQL/SQLViewExistsTest.vb
@@ -0,0 +1,54 @@
+﻿Imports System.Text
+Imports DatabaseObjects.SQL
+
+<TestClass()>
+Public Class SQLViewExistsTest
+
+    Public TestContext As TestContext
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("Views")>
+    Public Sub ViewExistsTSQL()
+
+        SQLStatement.DefaultConnectionType = Database.ConnectionType.SQLServer
+
+        Dim viewExists As New SQLViewExists("ViewName")
+
+        Assert.AreEqual(Of String)("SELECT * FROM [sysobjects] WHERE [Name] = 'ViewName' AND [XType] = 'V'", viewExists.SQL)
+
+    End Sub
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("Views")>
+    Public Sub ViewExistsMySQL()
+
+        SQLStatement.DefaultConnectionType = Database.ConnectionType.MySQL
+
+        Dim viewExists As New SQLViewExists("ViewName")
+
+        Assert.AreEqual(Of String)("SHOW VIEWS LIKE 'ViewName'", viewExists.SQL)
+
+    End Sub
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("Views")>
+    Public Sub ViewExistsHyperSQL()
+
+        SQLStatement.DefaultConnectionType = Database.ConnectionType.HyperSQL
+
+        Dim viewExists As New SQLViewExists("ViewName")
+
+        Assert.AreEqual(Of String)("SELECT * FROM ""INFORMATION_SCHEMA"".""VIEWS"" WHERE ""TABLE_SCHEMA"" = 'PUBLIC' AND ""TABLE_NAME"" = 'ViewName'", viewExists.SQL)
+
+    End Sub
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("Views")>
+    <ExpectedException(GetType(ArgumentNullException))>
+    Public Sub ViewExistsWithInvalidName()
+
+        Dim view As New SQLViewExists("")
+
+    End Sub
+
+End Class
