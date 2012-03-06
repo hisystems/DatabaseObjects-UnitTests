fa0a47bc06e44c6279f9f4d48b4a14a08bcda1c9
Toby Wicks <toby@hisystems.com.au>
2012-03-06 19:40:39 +1030

    Added tests for SQLConditionExpression.
    Associated commit DatabaseObjects:e6b0796ebd5e3d50851b3ea11804458c03461ef2

---
+++ SQL/SQLConditionExpressionTest.vb
@@ -0,0 +1,69 @@
+﻿Imports System.Text
+Imports DatabaseObjects.SQL
+
+<TestClass()>
+Public Class SQLConditionExpressionTest
+
+    Public TestContext As TestContext
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("SQLExpression")>
+    Public Sub SQLCondition()
+
+        Dim condition As New SQLConditionExpression(New SQLValueExpression(1), ComparisonOperator.LessThan, New SQLValueExpression(2))
+
+        Assert.AreEqual("1 < 2", condition.SQL(Database.ConnectionType.SQLServer))
+
+    End Sub
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("SQLExpression")>
+    Public Sub SQLConditionUsingProperties()
+
+        Dim condition As New SQLConditionExpression()
+        condition.LeftExpression = New SQLValueExpression(1)
+        condition.Compare = ComparisonOperator.LessThan
+        condition.RightExpression = New SQLValueExpression(2)
+
+        Assert.AreEqual("1 < 2", condition.SQL(Database.ConnectionType.SQLServer))
+
+    End Sub
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("SQLExpression")>
+    Public Sub SQLConditionWithSelectStatement()
+
+        SQLStatement.DefaultConnectionType = Database.ConnectionType.SQLServer
+
+        Dim selectStatement As New SQLSelect("Table1")
+        selectStatement.Where.Add(New SQLConditionExpression(New SQLValueExpression(1), ComparisonOperator.LessThan, New SQLValueExpression(2)))
+
+        Assert.AreEqual("SELECT * FROM [Table1] WHERE 1 < 2", selectStatement.SQL)
+
+    End Sub
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("SQLExpression")>
+    <ExpectedException(GetType(InvalidOperationException))>
+    Public Sub SQLConditionRightExpressionNotSet()
+
+        Dim condition As New SQLConditionExpression()
+        condition.LeftExpression = New SQLValueExpression(1)
+
+        condition.SQL(Database.ConnectionType.SQLServer)
+
+    End Sub
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("SQLExpression")>
+    <ExpectedException(GetType(InvalidOperationException))>
+    Public Sub SQLConditionLeftExpressionNotSet()
+
+        Dim condition As New SQLConditionExpression()
+        condition.RightExpression = New SQLValueExpression(2)
+
+        condition.SQL(Database.ConnectionType.SQLServer)
+
+    End Sub
+
+End Class
