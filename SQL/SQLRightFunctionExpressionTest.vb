0974e6dcd99ba14234f01f635d8f82ac4746563b
Toby Wicks <toby@hisystems.com.au>
2012-03-06 20:04:19 +1030

    Added tests for SQLRightFunctionExpression.
    Associated commit DatabaseObjcets: 520427aa62ce4b299243e0bab89a324adaa3d0b9

---
+++ SQL/SQLRightFunctionExpressionTest.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Text
+Imports DatabaseObjects.SQL
+
+<TestClass()>
+Public Class SQLRightFunctionExpressionTest
+
+    Public TestContext As TestContext
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("SQLExpression")>
+    Public Sub RightFunctionBasic()
+
+        Dim right As New SQLRightFunctionExpression("Field1", 2)
+
+        Assert.AreEqual("RIGHT([Field1], 2)", right.SQL(Database.ConnectionType.SQLServer))
+
+    End Sub
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("SQLExpression")>
+    <ExpectedException(GetType(ArgumentException))>
+    Public Sub RightFunctionWithInvalidLength()
+
+        Dim right As New SQLRightFunctionExpression("Field1", -1)
+
+    End Sub
+
+End Class
