0843994ff705fb99cb70365518d363fb92b43e41
Toby Wicks <toby@hisystems.com.au>
2012-03-22 20:33:30 +1030

    Added tests for disabling parentheses for SQLFunctionExpression when no parameters.
    Also setup test list for expressions and added tests for null parameters and DBNull.Value parameters.
    
    Associated commit DatabaseObjects: 691500c6a62f7cf6aca9bed0d699304c5ad81967

--- SQL/SQLFunctionExpression.vb
+++ SQL/SQLFunctionExpression.vb
@@ -12,7 +12,37 @@ Public Class SQLFunctionExpressionTest
 
         Dim function1 As New SQLFunctionExpression("SQLFunction")
 
+        Assert.AreEqual("SQLFunction", function1.SQL(Database.ConnectionType.SQLServer))
+
+    End Sub
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("SQLExpression")>
+    Public Sub SQLFunctionNoArgumentsWithParentheses()
+
+        Dim function1 As New SQLFunctionExpression("SQLFunction")
+        function1.IncludeParenthesesWhenArgumentsEmpty = True
+
         Assert.AreEqual("SQLFunction()", function1.SQL(Database.ConnectionType.SQLServer))
+
+    End Sub
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("SQLExpression")>
+    <ExpectedException(GetType(ArgumentNullException))>
+    Public Sub SQLFunctionWithEmptyArguments()
+
+        Dim function1 As New SQLFunctionExpression("SQLFunction", Nothing)
+
+    End Sub
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("SQLExpression")>
+    Public Sub SQLFunctionWithNullArgument()
+
+        Dim function1 As New SQLFunctionExpression("SQLFunction", New SQLValueExpression())
+
+        Assert.AreEqual("SQLFunction(NULL)", function1.SQL(Database.ConnectionType.SQLServer))
 
     End Sub
 
