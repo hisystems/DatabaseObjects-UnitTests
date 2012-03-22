29e338d5e854fbc50cb4bac0bf6440bf93096c9d
Toby Wicks <toby@hisystems.com.au>
2012-03-22 22:48:16 +1030

    Added tests for SQLLeftFunctionExpression.
    Associated commit DatabaseObjects: fb0d48b621bc6e0ce3ae7789f95ad90750917b6b

---
+++ SQL/SQLLeftFunctionExpressionTest.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Text
+Imports DatabaseObjects.SQL
+
+<TestClass()>
+Public Class SQLLeftFunctionExpressionTest
+
+    Public TestContext As TestContext
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("SQLExpression")>
+    Public Sub LeftFunctionBasic()
+
+        Dim left As New SQLLeftFunctionExpression("Field1", 2)
+
+        Assert.AreEqual("LEFT([Field1], 2)", left.SQL(Database.ConnectionType.SQLServer))
+
+    End Sub
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("SQLExpression")>
+    <ExpectedException(GetType(ArgumentException))>
+    Public Sub LeftFunctionWithInvalidLength()
+
+        Dim left As New SQLLeftFunctionExpression("Field1", -1)
+
+    End Sub
+
+End Class
