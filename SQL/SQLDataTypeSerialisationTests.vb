dc7d4cbf59621abb44f9076d585f6706388e8ef7
Toby Wicks <toby@hisystems.com.au>
2012-04-01 17:02:01 +0930

    Added tests for date/time serialisation.
    Associated commit DatabaseObjects: 75ced7d94392932eacadf93f7e1ff25f9eae7c7e

---
+++ SQL/SQLDataTypeSerialisationTests.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Text
+Imports DatabaseObjects.SQL
+
+<TestClass()>
+Public Class SQLDataTypeSerialisationTests
+
+    Public TestContext As TestContext
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("DataTypeSerialisation")>
+    Public Sub DateAndTime()
+
+        Dim dateAndTime As New DateTime(2000, 1, 2, 3, 4, 5, 6)
+
+        Dim insert As New SQLInsert
+        insert.TableName = "Table"
+        insert.ConnectionType = Database.ConnectionType.SQLServer
+        insert.Fields.Add("DateTimeField", dateAndTime)
+
+        Assert.AreEqual(Of String)("INSERT INTO [Table] ([DateTimeField]) VALUES ('2000-1-2 3:4:5.6')", insert.SQL)
+
+    End Sub
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("DataTypeSerialisation")>
+    Public Sub DateAndNoTime()
+
+        Dim dateAndTime As New DateTime(2000, 1, 2)
+
+        Dim insert As New SQLInsert
+        insert.TableName = "Table"
+        insert.ConnectionType = Database.ConnectionType.SQLServer
+        insert.Fields.Add("DateTimeField", dateAndTime)
+
+        Assert.AreEqual(Of String)("INSERT INTO [Table] ([DateTimeField]) VALUES ('2000-1-2')", insert.SQL)
+
+    End Sub
+
+End Class
