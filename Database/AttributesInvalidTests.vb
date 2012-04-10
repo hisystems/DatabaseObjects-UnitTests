b3719cf260f53c9a1f11462c4383bd19cd7fe73b
Toby Wicks <toby@hisystems.com.au>
2012-04-10 22:25:21 +0930

    Added tests for incorrect blank KeyFieldName when calling ObjectByKey or ObjectExists.
    Associated commit DatabaseObjects: 4a1a504a2ffe9704575770f23033b0de7904ea2d

---
+++ Database/AttributesInvalidTests.vb
@@ -0,0 +1,42 @@
+﻿Imports System.Configuration
+Imports DatabaseObjects.SQL
+
+<TestClass()>
+Public Class AttributesInvalidTests
+
+    Public Property TestContext As TestContext
+
+    Private Shared database As Database
+
+    <ClassInitialize()>
+    Public Shared Sub ClassInitialize(context As TestContext)
+
+        database = MicrosoftSQLServerDatabase.Parse(ConfigurationManager.ConnectionStrings("SQLServerTestDatabase").ConnectionString)
+
+    End Sub
+
+    <TestMethod()>
+    <TestCategory("Database")>
+    <ExpectedException(GetType(InvalidOperationException))>
+    Public Sub KeyFieldAttributeMissingObjectByKey()
+
+        'This collection does not have KeyFieldAttribute specified
+        Dim collection As New Generic.SimpleDatabaseObjects(database)
+
+        database.ObjectByKey(collection, "ABC")
+
+    End Sub
+
+    <TestMethod()>
+    <TestCategory("Database")>
+    <ExpectedException(GetType(InvalidOperationException))>
+    Public Sub KeyFieldAttributeMissingObjectExists()
+
+        'This collection does not have KeyFieldAttribute specified
+        Dim collection As New Generic.SimpleDatabaseObjects(database)
+
+        database.ObjectExists(collection, "ABC")
+
+    End Sub
+
+End Class
