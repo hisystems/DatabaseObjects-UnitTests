994796f660024b8b8df1cc972a6467014289132b
Toby Wicks <toby@hisystems.com.au>
2012-04-10 22:01:13 +0930

    Added tests for SQLAggregateExpression.
    Associated commit DatabaseObjects: 831dfa184fc37e0bf031a6c6a793afbfb044c1e8

---
+++ SQL/SQLAggregateExpressionTests.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Text
+Imports DatabaseObjects.SQL
+
+<TestClass()>
+Public Class SQLAggregateExpressionTests
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("SQLExpression")>
+    Public Sub AggregateFunctionAverage()
+
+        Dim aggregateExpression As New SQLAggregateExpression(AggregateFunction.Average, New SQLFieldExpression("Field1"))
+
+        Assert.AreEqual("AVG([Field1])", aggregateExpression.SQL(Database.ConnectionType.SQLServer))
+
+    End Sub
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("SQLExpression")>
+    Public Sub AggregateFunctionCount()
+
+        Dim aggregateExpression As New SQLAggregateExpression(AggregateFunction.Count, New SQLAllFieldsExpression)
+
+        Assert.AreEqual("COUNT(*)", aggregateExpression.SQL(Database.ConnectionType.SQLServer))
+
+    End Sub
+
+End Class
