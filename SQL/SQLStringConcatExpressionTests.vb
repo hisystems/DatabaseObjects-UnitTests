301b2e13bdeeffd920e62d41f0029fd053dbb304
Toby Wicks <toby@hisystems.com.au>
2012-04-20 20:01:19 +0930

    Added tests for concatenating string expressions.
    Associated commit DatabaseObjects: cdb485f42ef7bd33ee490132aaa576fad6cdb242

---
+++ SQL/SQLStringConcatExpressionTests.vb
@@ -0,0 +1,42 @@
+﻿Imports System.Text
+Imports DatabaseObjects.SQL
+
+<TestClass()>
+Public Class SQLStringConcatExpressionTests
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("SQLExpression")>
+    Public Sub StringConcatenate()
+
+        Dim concat As New SQLStringConcatExpression("LeftString", "RightString")
+
+        Assert.AreEqual("'LeftString' + 'RightString'", concat.SQL(Database.ConnectionType.SQLServer))
+
+    End Sub
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("SQLExpression")>
+    Public Sub StringConcatenateUsingExpressions()
+
+        Dim leftExpression As New SQLValueExpression("LeftString")
+        Dim rightExpression As New SQLFieldExpression("RightField")
+        Dim concat As New SQLStringConcatExpression(leftExpression, rightExpression)
+
+        Assert.AreEqual("'LeftString' + [RightField]", concat.SQL(Database.ConnectionType.SQLServer))
+
+    End Sub
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("SQLExpression")>
+    Public Sub StringConcatenateAll()
+
+        Dim concatAll = SQLStringConcatExpression.ConcatenateAll(
+            New SQLValueExpression("Expression1Value"), _
+            New SQLValueExpression("Expression2Value"), _
+            New SQLValueExpression("Expression3Value"))
+
+        Assert.AreEqual("'Expression1Value' + 'Expression2Value' + 'Expression3Value'", concatAll.SQL(Database.ConnectionType.SQLServer))
+
+    End Sub
+
+End Class
