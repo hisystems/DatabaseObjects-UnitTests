c45fca4779d17e84d1c4fd07ca512c73bad0152f
Toby Wicks <toby@hisystems.com.au>
2012-05-05 14:36:05 +0930

    Added tests for the DatabaseObjects UnitTestExtensions library.
    These tests are not really specific to testing the DatabaseObjects library so do not need to be run, but can be used for debugging issues in the DatabaseObjects.UnitTestExtensions library.

---
+++ UnitTestExtensions/DatabaseTestExtensionTesting.vb
@@ -0,0 +1,50 @@
+﻿Option Infer On
+
+Imports System.Text
+Imports System.Configuration
+Imports System.Linq
+Imports DatabaseObjects.SQL
+Imports DatabaseObjects.Exceptions
+Imports DatabaseObjects.UnitTestExtensions
+
+<DatabaseTestClass(ConnectionStringNames:={"SQLServerTestDatabase", "MySQLTestDatabase"})>
+Public Class DatabaseTestExtensionTesting
+
+    Public Property TestContext As TestContext
+
+    <TestInitialize()>
+    Public Sub TestInitialize()
+
+        TestContext.WriteLine("TestInitialize")
+
+    End Sub
+
+    <DatabaseTestInitialize()>
+    Public Sub DatabaseTestInitialize(database As Database)
+
+        TestContext.WriteLine("DatabaseTestInitialize")
+
+    End Sub
+
+    <TestMethod()>
+    Public Sub TestMethod(database As Database)
+
+        TestContext.WriteLine("TestMethod")
+
+    End Sub
+
+    <DatabaseTestCleanup()>
+    Public Sub DatabaseTestCleanup(database As Database)
+
+        TestContext.WriteLine("DatabaseTestCleanup")
+
+    End Sub
+
+    <TestCleanup()>
+    Public Sub TestCleanup()
+
+        TestContext.WriteLine("TestCleanup")
+
+    End Sub
+
+End Class
