fa1ba6cd0ccf6cb13629ad6d61cc65551850920a
Toby Wicks <toby@hisystems.com.au>
2012-05-05 15:44:50 +0930

    Added tests for SQLCreateView, SQLDropView and SQLViewExists. Closes #9. Closes #10.
    Renamed SQLViewExistsTest.vb to SQLViewTest.vb.
    
    Associated commit DatabaseObjects: 11686753788a2545d838940d09ffa95d67079a52

--- SQL/SQLViewTest.vb
+++ SQL/SQLViewTest.vb
@@ -1,10 +1,46 @@
 ﻿Imports System.Text
 Imports DatabaseObjects.SQL
+Imports DatabaseObjects.UnitTestExtensions
 
-<TestClass()>
-Public Class SQLViewExistsTest
+<DatabaseTestClass(ConnectionStringNames:={"SQLServerTestDatabase", "MySQLTestDatabase"})>
+Public Class SQLViewTest
 
     Public TestContext As TestContext
+
+    <DatabaseTestInitialize()>
+    Public Sub DatabaseTestInitialize(database As Database)
+
+        Using connection = New ConnectionScope(database)
+            If Not connection.Execute(New SQLTableExists("Table1")).Read Then
+                Dim createTable As New SQLCreateTable
+                createTable.Name = "Table1"
+                createTable.Fields.Add("Field1", DataType.Integer)
+                connection.Execute(New SQLDropTable(createTable.Name))
+            End If
+
+            If connection.Execute(New SQLViewExists("View1")).Read Then
+                connection.Execute(New SQLDropView("View1"))
+            End If
+        End Using
+
+    End Sub
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("TableSchema")>
+    Public Sub CreateView(database As Database)
+
+        Dim selectStatement As New SQLSelect
+        selectStatement.Tables.Add("Table1")
+
+        Dim createView As New SQLCreateView
+        createView.Name = "View1"
+        createView.Select = selectStatement
+
+        Using connection = New ConnectionScope(database)
+            connection.Execute(createView)
+        End Using
+
+    End Sub
 
     <TestMethod()>
     <TestCategory("SQL"), TestCategory("Views")>
