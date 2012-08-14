b7787835a9780c722b4c1ba41515a9b6909ddb75
Toby Wicks <toby@hisystems.com.au>
2012-08-14 15:39:02 +0930

    Added tests for SQL Logical Expression
    Associated commit DatabaseObjects:
    e1c97ef999566fee72fa117f47e21f0b15d29e08

---
+++ SQL/SQLLogicalExpressionTests.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Text
+Imports DatabaseObjects.SQL
+Imports DatabaseObjects.SQL.Serializers
+
+<TestClass()>
+Public Class SQLLogicalExpressionTests
+
+    Public TestContext As TestContext
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("SQLExpression")>
+    Public Sub LogicalAndExpression()
+
+        Dim logicalExpression As New SQLLogicalExpression(New SQLFieldExpression("F1"), LogicalOperator.And, New SQLFieldExpression("F2"))
+
+        Assert.AreEqual("([F1] AND [F2])", logicalExpression.SQL(Serializers.Items(Database.ConnectionType.SQLServer)))
+
+    End Sub
+    
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("SQLExpression")>
+    Public Sub LogicalOrExpression()
+
+        Dim logicalExpression As New SQLLogicalExpression(New SQLFieldExpression("F1"), LogicalOperator.Or, New SQLFieldExpression("F2"))
+
+        Assert.AreEqual("([F1] OR [F2])", logicalExpression.SQL(Serializers.Items(Database.ConnectionType.SQLServer)))
+
+    End Sub
+
+End Class
