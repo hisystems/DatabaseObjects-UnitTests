8c22a371ef600a36385fbd014dbe3d3a7f198908
Toby Wicks <toby@hisystems.com.au>
2012-09-01 14:44:01 +0930

    Added tests for SQLCastExpression
    Associated commit DatabaseObjects:
    0a14e43c431698c8cf7dfcd5178f6cf9a3c27bdf

---
+++ SQL/SQLCastExpressionTests.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Text
+Imports DatabaseObjects.SQL
+Imports DatabaseObjects.SQL.Serializers
+
+<TestClass()>
+Public Class SQLCastExpressionTests
+
+    Public TestContext As TestContext
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("SQLExpression")>
+    Public Sub CastExpression()
+
+        Dim castExpression As New SQLCastExpression("Field1", DataType.UnicodeVariableCharacter, 10)
+
+        Assert.AreEqual("CAST([Field1] AS NVARCHAR(10))", castExpression.SQL(Serializers.Items(Database.ConnectionType.SQLServer)))
+
+    End Sub
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("SQLExpression")>
+    Public Sub CastExpressionNonString()
+
+        Dim castExpression As New SQLCastExpression("Field1", DataType.DateTime)
+
+        Assert.AreEqual("CAST([Field1] AS DATETIME)", castExpression.SQL(Serializers.Items(Database.ConnectionType.SQLServer)))
+
+    End Sub
+
+End Class
