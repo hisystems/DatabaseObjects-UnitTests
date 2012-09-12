47e1dd3122bfee9069a7be92dd6ab87a44708744
Toby Wicks <toby@hisystems.com.au>
2012-09-12 09:35:39 +0930

    Added test for writing time component when just milliseconds.
    Associated commit DatabaseObjects:
    5c28145b35b6ee86fb554bad503f3a2c1b376774

--- SQL/SQLDataTypeSerialisationTests.vb
+++ SQL/SQLDataTypeSerialisationTests.vb
@@ -23,6 +23,21 @@ Public Class SQLDataTypeSerialisationTests
 
     <TestMethod()>
     <TestCategory("SQL"), TestCategory("DataTypeSerialisation")>
+    Public Sub DateAndTimeWithOnlyMilliseconds()
+
+        Dim dateAndTime As New DateTime(2000, 1, 2, Hour:=0, Minute:=0, Second:=0, millisecond:=6)
+
+        Dim insert As New SQLInsert
+        insert.TableName = "Table"
+        insert.ConnectionType = Database.ConnectionType.SQLServer
+        insert.Fields.Add("DateTimeField", dateAndTime)
+
+        Assert.AreEqual(Of String)("INSERT INTO [Table] ([DateTimeField]) VALUES ('2000-01-02 00:00:00.006')", insert.SQL)
+
+    End Sub
+
+    <TestMethod()>
+    <TestCategory("SQL"), TestCategory("DataTypeSerialisation")>
     Public Sub DateAndNoTime()
 
         Dim dateAndTime As New DateTime(2000, 1, 2)
