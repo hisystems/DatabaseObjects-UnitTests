7aa13a1aff81e4fb40fb80b1fc27092abb14cd62
Toby Wicks <toby@hisystems.com.au>
2012-10-05 14:11:24 +0930

    SQLIndexTests changed indentation from spaces to tabs.
    No functional changes. Only formatting changes.

--- SQL/SQLIndexTests.vb
+++ SQL/SQLIndexTests.vb
@@ -5,44 +5,44 @@ Imports DatabaseObjects.UnitTestExtensions
 <DatabaseTestClass(ConnectionStringNames:={"SQLServerTestDatabase", "MySQLTestDatabase", "SQLiteTestDatabase"})>
 Public Class SQLIndexTests
 
-    <DatabaseTestInitialize()>
-    Public Sub DatabaseTestInitialize(database As Database)
+	<DatabaseTestInitialize()>
+	Public Sub DatabaseTestInitialize(database As Database)
 
-        Dim createTable As New SQLCreateTable
-        createTable.Name = "Table1"
-        createTable.Fields.Add("Field1", DataType.Integer)
-        createTable.Fields.Add("Field2", DataType.Integer)
+		Dim createTable As New SQLCreateTable
+		createTable.Name = "Table1"
+		createTable.Fields.Add("Field1", DataType.Integer)
+		createTable.Fields.Add("Field2", DataType.Integer)
 
-        database.RecreateTable(createTable)
+		database.RecreateTable(createTable)
 
-    End Sub
+	End Sub
 
-    <TestMethod()>
-    <TestCategory("SQL"), TestCategory("TableSchema")>
-    Public Sub CreateIndex(database As Database)
+	<TestMethod()>
+	<TestCategory("SQL"), TestCategory("TableSchema")>
+	Public Sub CreateIndex(database As Database)
 
-        Dim createIndex As New SQLCreateIndex
-        createIndex.Name = "Index1"
-        createIndex.TableName = "Table1"
-        createIndex.Fields.Add("Field1")
-        createIndex.Fields.Add("Field2")
+		Dim createIndex As New SQLCreateIndex
+		createIndex.Name = "Index1"
+		createIndex.TableName = "Table1"
+		createIndex.Fields.Add("Field1")
+		createIndex.Fields.Add("Field2")
 
-        Using connection = New ConnectionScope(database)
-            connection.Execute(createIndex)
-        End Using
+		Using connection = New ConnectionScope(database)
+			connection.Execute(createIndex)
+		End Using
 
-    End Sub
+	End Sub
 
-    <TestMethod()>
-    <TestCategory("SQL"), TestCategory("TableSchema")>
-    Public Sub DropIndex(database As Database)
+	<TestMethod()>
+	<TestCategory("SQL"), TestCategory("TableSchema")>
+	Public Sub DropIndex(database As Database)
 
-        CreateIndex(database)
+		CreateIndex(database)
 
-        Using connection = New ConnectionScope(database)
-            connection.Execute(New SQLDropIndex("Index1", "Table1"))
-        End Using
+		Using connection = New ConnectionScope(database)
+			connection.Execute(New SQLDropIndex("Index1", "Table1"))
+		End Using
 
-    End Sub
+	End Sub
 
 End Class
