5a000d49da8b4d8fd2efdebb1959538c9132bcde
Toby Wicks <toby@hisystems.com.au>
2012-10-05 10:55:02 +0930

    Updated test for ensuring DatabaseTestClassAttribute ConnectionStrings
    Associated commit DatabseObjects-UnitTestExtensions:
    98d664b53206c030409d2b2e44be0dd3ecf362ec

--- UnitTestExtensions/DatabaseTestExtensionTesting.vb
+++ UnitTestExtensions/DatabaseTestExtensionTesting.vb
@@ -11,6 +11,7 @@ Imports DatabaseObjects.UnitTestExtensions
 Public Class DatabaseTestExtensionTesting
 
     Public Property TestContext As TestContext
+    Private TestMethodExecutionCount As Integer = 0
 
     <TestInitialize()>
     Public Sub TestInitialize()
@@ -30,6 +31,7 @@ Public Class DatabaseTestExtensionTesting
     Public Sub TestMethod(database As Database)
 
         TestContext.WriteLine("TestMethod")
+        TestMethodExecutionCount += 1
 
     End Sub
 
@@ -44,6 +46,57 @@ Public Class DatabaseTestExtensionTesting
     Public Sub TestCleanup()
 
         TestContext.WriteLine("TestCleanup")
+
+        'Should be executed 3 times - once for each database
+        Assert.AreEqual(3, TestMethodExecutionCount)
+
+    End Sub
+
+End Class
+
+
+<DatabaseTestClass(ConnectionStringNames:={"SQLServerTestDatabase"})>
+Public Class DatabaseTestExtensionTestingOnlySQLServer
+
+    Public Property TestContext As TestContext
+    Private TestMethodExecutionCount As Integer = 0
+
+    <TestInitialize()>
+    Public Sub TestInitialize()
+
+        TestContext.WriteLine("TestInitialize")
+
+    End Sub
+
+    <DatabaseTestInitialize()>
+    Public Sub DatabaseTestInitialize(database As Database)
+
+        TestContext.WriteLine("DatabaseTestInitialize")
+
+    End Sub
+
+    <TestMethod()>
+    Public Sub TestMethod(database As Database)
+
+        TestContext.WriteLine("TestMethod")
+        TestMethodExecutionCount += 1
+
+    End Sub
+
+    <DatabaseTestCleanup()>
+    Public Sub DatabaseTestCleanup(database As Database)
+
+        TestContext.WriteLine("DatabaseTestCleanup")
+
+    End Sub
+
+    <TestCleanup()>
+    Public Sub TestCleanup()
+
+        TestContext.WriteLine("TestCleanup")
+
+        'Should be executed once for the SQL server database
+        Assert.AreEqual(1, TestMethodExecutionCount)
 
     End Sub
 
