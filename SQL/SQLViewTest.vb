2acb757c99f8079936806f9bcc4b166a74d31ab7
Toby Wicks <toby@hisystems.com.au>
2012-10-05 15:30:25 +0930

    SQLViewTest changed indentation from spaces to tabs.

--- SQL/SQLViewTest.vb
+++ SQL/SQLViewTest.vb
@@ -5,56 +5,56 @@ Imports DatabaseObjects.UnitTestExtensions
 <DatabaseTestClass(ConnectionStringNames:={"SQLServerTestDatabase", "MySQLTestDatabase", "SQLiteTestDatabase"})>
 Public Class SQLViewTest
 
-    Public TestContext As TestContext
+	Public TestContext As TestContext
 
-    <DatabaseTestInitialize()>
-    Public Sub DatabaseTestInitialize(database As Database)
+	<DatabaseTestInitialize()>
+	Public Sub DatabaseTestInitialize(database As Database)
 
-        Dim createTable As New SQLCreateTable
-        createTable.Name = "Table1"
-        createTable.Fields.Add("Field1", DataType.Integer)
+		Dim createTable As New SQLCreateTable
+		createTable.Name = "Table1"
+		createTable.Fields.Add("Field1", DataType.Integer)
 
-        database.RecreateTable(createTable)
-        database.DropViewIfExists("View1")
+		database.RecreateTable(createTable)
+		database.DropViewIfExists("View1")
 
-    End Sub
+	End Sub
 
-    <TestMethod()>
-    <TestCategory("SQL"), TestCategory("TableSchema")>
-    Public Sub CreateView(database As Database)
+	<TestMethod()>
+	<TestCategory("SQL"), TestCategory("TableSchema")>
+	Public Sub CreateView(database As Database)
 
-        Dim selectStatement As New SQLSelect
-        selectStatement.Tables.Add("Table1")
+		Dim selectStatement As New SQLSelect
+		selectStatement.Tables.Add("Table1")
 
-        Dim createView As New SQLCreateView
-        createView.Name = "View1"
-        createView.Select = selectStatement
+		Dim createView As New SQLCreateView
+		createView.Name = "View1"
+		createView.Select = selectStatement
 
-        Using connection = New ConnectionScope(database)
-            connection.Execute(createView)
-        End Using
+		Using connection = New ConnectionScope(database)
+			connection.Execute(createView)
+		End Using
 
-    End Sub
+	End Sub
 
-    <TestMethod()>
-    <TestCategory("SQL"), TestCategory("Views")>
-    Public Sub ViewExistsHyperSQL()
+	<TestMethod()>
+	<TestCategory("SQL"), TestCategory("Views")>
+	Public Sub ViewExistsHyperSQL()
 
-        SQLStatement.DefaultConnectionType = Database.ConnectionType.HyperSQL
+		SQLStatement.DefaultConnectionType = Database.ConnectionType.HyperSQL
 
-        Dim viewExists As New SQLViewExists("ViewName")
+		Dim viewExists As New SQLViewExists("ViewName")
 
-        Assert.AreEqual(Of String)("SELECT * FROM ""INFORMATION_SCHEMA"".""VIEWS"" WHERE ""TABLE_SCHEMA"" = 'PUBLIC' AND ""TABLE_NAME"" = 'ViewName'", viewExists.SQL)
+		Assert.AreEqual(Of String)("SELECT * FROM ""INFORMATION_SCHEMA"".""VIEWS"" WHERE ""TABLE_SCHEMA"" = 'PUBLIC' AND ""TABLE_NAME"" = 'ViewName'", viewExists.SQL)
 
-    End Sub
+	End Sub
 
-    <TestMethod()>
-    <TestCategory("SQL"), TestCategory("Views")>
-    <ExpectedException(GetType(ArgumentNullException))>
-    Public Sub ViewExistsWithInvalidName()
+	<TestMethod()>
+	<TestCategory("SQL"), TestCategory("Views")>
+	<ExpectedException(GetType(ArgumentNullException))>
+	Public Sub ViewExistsWithInvalidName()
 
-        Dim view As New SQLViewExists("")
+		Dim view As New SQLViewExists("")
 
-    End Sub
+	End Sub
 
 End Class
